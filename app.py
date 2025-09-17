ab94ebb0023f7693f0cf406ab48ea3f98983529a
lfelipeos <lfods.bf2@gmail.com>
2025-09-17 20:56:28 -0300

    Segundo commit: add a função para gerar um arquivo 'resultado.csv' com o resultado da análise.

--- app.py
+++ app.py
@@ -8,4 +8,6 @@ st.write("Coloque os currículos na pasta 'curriculos' e clique abaixo")
 if st.button("Analisar"):
     df = processar_pasta('curriculos')
     st.dataframe(df)
+    df.to_csv('resultado.csv', index=False)
+    st.success("Arquivo resultado.csv gerado com sucesso!")
     st.success("Análise Concluída!")
